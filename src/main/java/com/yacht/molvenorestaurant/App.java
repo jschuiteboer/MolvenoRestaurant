0779141cb3a856320f19edafde91840eed6082ce
Agroeningen <37032183+Agroeningen@users.noreply.github.com>
2018-03-14 15:37:24 +0100

    Bill and Guest
    added bill and guest w/ bigDecimal

--- src/main/java/com/yacht/molvenorestaurant/App.java
+++ src/main/java/com/yacht/molvenorestaurant/App.java
@@ -1,7 +1,11 @@
 package com.yacht.molvenorestaurant;
 
+import com.yacht.molvenorestaurant.business.Dish;
+
 public class App {
     public static void main(String[] args) {
+
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/com/yacht/molvenorestaurant/business/Bill.java
@@ -0,0 +1,57 @@
+package com.yacht.molvenorestaurant.business;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Bill {
+    private boolean isPaid;
+    private Table table = new Table();
+    private Guest guest = new Guest();
+    private List<Dish> dishList;
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    public List<Dish> getDishList() {
+        return dishList;
+    }
+
+    public void setDishList(List<Dish> dishList) {
+        this.dishList = dishList;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public BigDecimal getTotalPrice(){
+        BigDecimal total = new BigDecimal(0);
+        for(Dish dish: dishList) {
+            total = total.add(dish.getPrice());
+        }
+        return total;
+    }
+
+    public void getTableNumber(){
+
+    }
+}
