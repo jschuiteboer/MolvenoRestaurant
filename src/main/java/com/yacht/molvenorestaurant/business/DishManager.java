dd87a492065aeb196bc06b91fcf3b85842787e3d
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-03-15 16:00:07 +0100

    2e version

--- src/main/java/com/yacht/molvenorestaurant/business/DishManager.java
+++ src/main/java/com/yacht/molvenorestaurant/business/DishManager.java
@@ -7,5 +7,22 @@ import java.util.*;
 public class DishManager {
     private ArrayList<Dish> dishList;
 
-    
+    public void addDish(Dish dish){
+        dishList.add(dish);
+    }
+    public void removeDish(Dish dish){
+        dishList.remove(dish);
+    }
+
+    public ArrayList<Dish> getDishList() {
+        return dishList;
+    }
+
+    public void updateDish(Dish Dish, String name){
+        for (Dish oldDish : dishList) {
+            if (oldDish.getName().equals(name)) {
+                dishList.add(dishList.indexOf(oldDish), Dish);
+            }
+        }
+    }
 }
