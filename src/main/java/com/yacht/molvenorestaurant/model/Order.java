d630d4d2ac20472f1be87bb94a3a572ad5282f7d
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-03-15 14:54:39 +0100

    order

--- src/main/java/com/yacht/molvenorestaurant/model/Order.java
+++ src/main/java/com/yacht/molvenorestaurant/model/Order.java
@@ -2,17 +2,51 @@ package com.yacht.molvenorestaurant.model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 public class Order {
-    private Guest guest;
-
     private List<Dish> orderList;
-
-    private boolean onAccount;
-
-    private int tableNumber;
-
     private String comment;
+    private boolean isReady;
+
+    public BigDecimal getTotalPrice()
+    {
+        BigDecimal totalPrice = new BigDecimal(0);
+
+        for (Dish dish: orderList) {
+            BigDecimal price = new BigDecimal(dish.getPrice());
+            totalPrice = totalPrice.add(price);
+        }
+
+        return totalPrice;
+    }
+
+    public void addDish(Dish dish)
+    {}
+
+    public List<Dish> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Dish> orderList) {
+        this.orderList = orderList;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public boolean isReady() {
+        return isReady;
+    }
+
+    public void setReady(boolean ready) {
+        isReady = ready;
+    }
 }
