75df0c61dd82853437917c266b32262c5d928a48
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-03-15 15:24:48 +0100

    OrderManager

---
+++ src/main/java/com/yacht/molvenorestaurant/business/OrderManager.java
@@ -0,0 +1,34 @@
+package com.yacht.molvenorestaurant.business;
+
+import com.yacht.molvenorestaurant.model.Order;
+
+import java.util.ArrayList;
+
+public class OrderManager {
+
+    public ArrayList<Order> orders;
+
+    public void addOrder(Order order){
+        orders.add(order);
+    }
+
+    public void removeOrder(Order order){
+        orders.remove(order);
+    }
+
+    public void updateOrder(Order order, Long ID){
+        for (Order oldOrder : orders) {
+            if (oldOrder.getID().equals(ID)) {
+                orders.add(orders.indexOf(oldOrder), order);
+            }
+        }
+    }
+
+    public void markOrderComplete(Order order){
+
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+}
