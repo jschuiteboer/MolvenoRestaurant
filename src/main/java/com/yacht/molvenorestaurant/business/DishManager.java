95174b85443d886beb040ef2c153d71a0bba4605
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-03-15 15:51:21 +0100

    1e versie

---
+++ src/main/java/com/yacht/molvenorestaurant/business/DishManager.java
@@ -0,0 +1,11 @@
+package com.yacht.molvenorestaurant.business;
+
+import com.yacht.molvenorestaurant.model.Dish;
+
+import java.util.*;
+
+public class DishManager {
+    private ArrayList<Dish> dishList;
+
+    
+}
