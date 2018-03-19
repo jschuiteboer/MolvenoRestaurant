22a36008fe2a2029b707d7334eee267e94ff0a7c
Lars <lars.tebooy@hotmail.com>
2018-03-19 15:35:35 +0100

    Added Pooier Supplier

---
+++ src/main/java/com/yacht/molvenorestaurant/model/Suplier.java
@@ -0,0 +1,16 @@
+package com.yacht.molvenorestaurant.model;
+
+public class Suplier {
+
+    private String name;
+
+    public Suplier(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
