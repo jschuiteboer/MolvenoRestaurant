3bd317e41d0f339cdb399a40731103b5150ffb32
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-03-25 15:35:14 +0200

    Made IOrderRepository extend CrudRepository. Removed page endpoint in order controller.

--- src/main/java/com/yacht/molvenorestaurant/business/OrderManager.java
+++ src/main/java/com/yacht/molvenorestaurant/business/OrderManager.java
@@ -3,18 +3,12 @@ package com.yacht.molvenorestaurant.business;
 import com.yacht.molvenorestaurant.model.Order;
 import com.yacht.molvenorestaurant.repository.IOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderManager {
     @Autowired
     private IOrderRepository orderRepository;
-
-    public Page<Order> getOrders(PageRequest request) {
-        return orderRepository.findAll(request);
-    }
 
     public Iterable<Order> getAll() {
         return orderRepository.findAll();
--- src/main/java/com/yacht/molvenorestaurant/controller/OrderController.java
+++ src/main/java/com/yacht/molvenorestaurant/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.yacht.molvenorestaurant.controller;
 import com.yacht.molvenorestaurant.business.OrderManager;
 import com.yacht.molvenorestaurant.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,11 +16,6 @@ public class OrderController {
     @GetMapping()
     public Iterable<Order> getAll() {
         return this.orderManager.getAll();
-    }
-
-    @GetMapping("page/{page}")
-    public Iterable<Order> getPage(@PathVariable int page) {
-        return this.orderManager.getOrders(new PageRequest(page, PAGE_SIZE));
     }
 
     @PostMapping("save")
