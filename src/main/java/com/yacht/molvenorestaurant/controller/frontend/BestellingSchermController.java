50495372616b4a8378a2b3c1715fe363ab837f1d
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-03-26 16:19:23 +0200

    Added mustache dependency. Moved static html to template

---
+++ src/main/java/com/yacht/molvenorestaurant/controller/frontend/BestellingSchermController.java
@@ -0,0 +1,17 @@
+package com.yacht.molvenorestaurant.controller.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("admin/bestellingen")
+public class BestellingSchermController {
+
+    @GetMapping()
+    public String index(Map<String, Object> model){
+        return "bestellingscherm";
+    }
+}
