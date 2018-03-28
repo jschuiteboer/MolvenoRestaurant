51936b809bc1bb414e89bdd843f28338e67e4638
Lars <lars.tebooy@hotmail.com>
2018-03-28 16:23:10 +0200

    added modal for adding dish

--- src/main/resources/static/js/changeMenuScreen.js
+++ src/main/resources/static/js/changeMenuScreen.js
@@ -5,8 +5,8 @@ var _restEndpoint = '/api/dishes/';
     var _modalElement = $('#entryModal');
 
     // AddDish Modal
-    var _addDishModal = $('#addDishModal');
     var _dishButton = $('#addDishBtn');
+    var isButtonClicked = false;
 
     var _dataTable = _tableElement.DataTable({
         ajax: {
@@ -15,6 +15,17 @@ var _restEndpoint = '/api/dishes/';
             type: "GET",
         },
     });
+
+    console.log(isButtonClicked);
+
+    _dishButton.on('click', function(){
+        console.log(isButtonClicked);
+
+        var data = _dataTable.row(this).data();
+
+        openModalForAddDish(data);
+    });
+
 
     _tableElement.on('click', 'tr', function () {
         _tableElement.find('tr.selected').removeClass('selected');
@@ -34,6 +45,46 @@ var _restEndpoint = '/api/dishes/';
         });
     });
 
+    //geef data aan modal voor add dish
+    function openModalForAddDish(data) {
+          var _nameField = _modalElement.find('#name');
+          var _priceField = _modalElement.find('#price');
+          var _descriptionField = _modalElement.find('#description');
+
+            _nameField.val("");
+            _priceField.val("");
+            _descriptionField.val("");
+
+          _modalElement.find('#modal-title').html('Add Dish');
+
+          _modalElement.find('#btnsubmit')
+          .off('click')
+          .on('click', function() {
+              var saveData = {
+                  name: _nameField.val(),
+                  price: _priceField.val(),
+                  description: _descriptionField.val()
+              };
+
+              $.ajax({
+                  contentType : 'application/json',
+                  url: _restEndpoint,
+                  type: 'post',
+                  data: JSON.stringify(saveData),
+                  success: function() {
+                      _modalElement.modal('hide');
+                      reloadData();
+                  },
+              });
+          });
+
+          _modalElement.find('#btndelete').hide();
+
+
+          _modalElement.modal('show');
+    }
+
+
     function openModalForObject(data) {
         var _nameField = _modalElement.find('#name');
         var _priceField = _modalElement.find('#price');
@@ -43,7 +94,7 @@ var _restEndpoint = '/api/dishes/';
         _priceField.val(data.price);
         _descriptionField.val(data.description);
 
-        _modalElement.find('#modal-title').html('Edit Ingredient');
+        _modalElement.find('#modal-title').html('Edit Dish');
 
         _modalElement.find('#btnsubmit')
         .off('click')
@@ -55,19 +106,20 @@ var _restEndpoint = '/api/dishes/';
                 description: _descriptionField.val()
             };
 
-        $.ajax({
-            contentType : 'application/json',
-            url: _restEndpoint,
-            type: 'post',
-            data: JSON.stringify(saveData),
-            success: function() {
-                _modalElement.modal('hide');
-                reloadData();
-            },
+            $.ajax({
+                contentType : 'application/json',
+                url: _restEndpoint,
+                type: 'post',
+                data: JSON.stringify(saveData),
+                success: function() {
+                    _modalElement.modal('hide');
+                    reloadData();
+                },
+            });
         });
-    });
 
-    _modalElement.find('#btndelete')
+
+    _modalElement.find('#btndelete').show()
     .off('click')
     .on('click', function() {
         var result = confirm('this action can not be undone');
