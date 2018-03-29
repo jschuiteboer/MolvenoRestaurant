b2aa2d9908b8057764948592a249f1a79f65233d
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-03-29 20:51:21 +0200

    Write h2 database to file. Removed @PostContruct test data initialization.

--- src/main/java/com/yacht/molvenorestaurant/business/IngredientManager.java
+++ src/main/java/com/yacht/molvenorestaurant/business/IngredientManager.java
@@ -1,13 +1,9 @@
 package com.yacht.molvenorestaurant.business;
 
-import com.yacht.molvenorestaurant.model.EAllergy;
 import com.yacht.molvenorestaurant.model.Ingredient;
 import com.yacht.molvenorestaurant.repository.IIngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 
 @Component
 public class IngredientManager {
@@ -26,20 +22,6 @@ public class IngredientManager {
 
     public void deleteIngredient(Long id) {
         this.ingredientRepository.delete(id);
-    }
-
-    @PostConstruct
-    private void createSomeTestData() {
-        ArrayList<Ingredient> Inventory = new ArrayList<Ingredient>();
-        EAllergy milkAllergy = (EAllergy.MILK);
-        EAllergy glutenAllergy = (EAllergy.GLUTEN);
-
-        Ingredient kaas = new Ingredient("Kaas", 30, 80,milkAllergy, null);
-        Ingredient melk = new Ingredient("Melk", 20, 80, milkAllergy, null);
-        Ingredient bloem = new Ingredient("Bloem", 50, 80,glutenAllergy,null);
-        Ingredient boter = new Ingredient("Boter", 10, 40, milkAllergy,null);
-
-        this.saveIngredient(kaas); this.saveIngredient(melk); this.saveIngredient(bloem); this.saveIngredient(boter);
     }
 }
 
