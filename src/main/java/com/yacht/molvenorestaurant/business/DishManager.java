e26c7046259cb3e961755961f3dedf9b3521cce7
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-04-04 16:10:25 +0200

    first draft

--- src/main/java/com/yacht/molvenorestaurant/business/DishManager.java
+++ src/main/java/com/yacht/molvenorestaurant/business/DishManager.java
@@ -1,13 +1,28 @@
 package com.yacht.molvenorestaurant.business;
 
 import com.yacht.molvenorestaurant.model.Dish;
+import com.yacht.molvenorestaurant.model.Ingredient;
 import com.yacht.molvenorestaurant.repository.IDishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class DishManager {
 
+    //produces a suggested price from all the selected ingredients
+    //by myltiplying the price with the amount for eacht ingredient
+    public BigDecimal getSuggestedPrice(Dish dish){
+        BigDecimal suggestion = new BigDecimal(0);
+
+        for (Ingredient ingredient: this.getIngredientList()) {
+            BigDecimal ingredientPrice = (ingredient.getPrice)*quantity;
+            suggestion = ingredientPrice.add(suggestion);
+        }
+
+        return suggestion;
+    }
     @Autowired
     private IDishRepository dishRepository;
 
