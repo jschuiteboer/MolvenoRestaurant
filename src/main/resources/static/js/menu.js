1936e4c2469b3478675e49cd03c3797d019e5fbd
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-09 15:25:15 +0200

    changed layout. Implemented adding dish to order

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,39 +1,52 @@
-var _entryModal = $('#entryModal');
-var _priceContainer = _entryModal.find('#price-container');
-var _ingredientTable = _entryModal.find('#ingredientTable').DataTable({
-    searching: false,
-    paging: false,
-});
+$(document).ready(function() {
+    var _entryModal = $('#entryModal');
+    var _priceContainer = _entryModal.find('#price-container');
+    var _ingredientTable = _entryModal.find('#ingredientTable').DataTable({
+        searching: false,
+        paging: false,
+    });
+    var _btnAddtoOrder = _entryModal.find('#btnAddtoOrder');
 
-$('.dish-table').each(function(i, _tableElement) {
-    _tableElement = $(_tableElement);
-    var endPoint = '/api/' + _tableElement.data('endpoint');
-
-    var _dishDataTable = _tableElement.DataTable({
-        ajax: {
-            url: endPoint,
-            dataSrc: "",
-            type: "GET",
-
-        },
+    var _orderTable = $('#orderTable').DataTable({
         searching: false,
         paging: false,
     });
 
+    $('.dish-table').each(function(i, _tableElement) {
+        _tableElement = $(_tableElement);
+        var endPoint = '/api/' + _tableElement.data('endpoint');
 
-    _tableElement.on('click', 'tr', function () {
-        var data = _dishDataTable.row(this).data();
+        var _dishDataTable = _tableElement.DataTable({
+            ajax: {
+                url: endPoint,
+                dataSrc: "",
+                type: "GET",
 
-        openModalForDish(data);
+            },
+            searching: false,
+            paging: false,
+        });
+
+        _tableElement.on('click', 'tr', function () {
+            var data = _dishDataTable.row(this).data();
+
+            openModalForDish(data);
+        });
+
+        function openModalForDish(dish) {
+            _ingredientTable.clear();
+            _ingredientTable.rows.add(dish.ingredientList);
+            _ingredientTable.draw();
+
+            _priceContainer.text(dish.price);
+
+            _btnAddtoOrder.off('click').on('click', function() {
+                _orderTable.row.add(dish);
+                _orderTable.draw();
+                _entryModal.modal('hide');
+            });
+
+            _entryModal.modal('show');
+        }
     });
-
-    function openModalForDish(dish) {
-        _ingredientTable.clear();
-        _ingredientTable.rows.add(dish.ingredientList);
-        _ingredientTable.draw();
-
-        _priceContainer.text(dish.price);
-
-        _entryModal.modal('show');
-    }
 });
