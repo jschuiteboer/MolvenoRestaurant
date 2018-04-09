007609ac0ea534b1760fabe81bdb396860fa40ca
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-09 14:14:43 +0200

    reduced menu js code

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,76 +1,17 @@
-var _restEndpoint = '/api/dishes';
-var _tableElementStarter = $('#starterTable');
-var _tableElementMain = $('#mainTable');
-var _tableElementDessert = $('#dessertTable');
-var _tableElementDrinks = $('#drinksTable');
-var _tableElementAlcohol = $('#alcoholTable');
+$('.dish-table').each(function(i, _tableElement) {
+    _tableElement = $(_tableElement);
+    var endPoint = '/api/' + _tableElement.data('endpoint');
 
+    _tableElement.DataTable({
+        ajax: {
+            url: endPoint,
+            dataSrc: "",
+            type: "GET",
 
-// Define Modal
-var _modalElement = $('#entryModal');
-
-
-var _dataTable = _tableElementStarter.DataTable({
-    ajax: {
-
-        url: _restEndpoint + "/cat/Starter",
-        dataSrc: "",
-        type: "GET",
-
-    },
-    searching: false,
-    paging: false,
-});
-
-var _dataTable = _tableElementMain.DataTable({
-    ajax: {
-
-        url: _restEndpoint + "/cat/Main",
-        dataSrc: "",
-        type: "GET",
-
-    },
-    searching: false,
+        },
+        searching: false,
         paging: false,
+    });
+
+    
 });
-
-var _dataTable = _tableElementDessert.DataTable({
-    ajax: {
-
-        url: _restEndpoint + "/cat/Dessert",
-        dataSrc: "",
-        type: "GET",
-
-    },
-    searching: false,
-        paging: false,
-});
-
-var _dataTable = _tableElementDrinks.DataTable({
-    ajax: {
-
-        url: _restEndpoint + "/cat/NonAlcoholicDrinks",
-        dataSrc: "",
-        type: "GET",
-
-    },
-    searching: false,
-        paging: false,
-});
-
-var _dataTable = _tableElementAlcohol.DataTable({
-    ajax: {
-
-        url: _restEndpoint + "/cat/AlcoholicDrinks",
-        dataSrc: "",
-        type: "GET",
-
-    },
-    searching: false,
-        paging: false,
-});
-
-
-function reloadData() {
-    _dataTable.ajax.reload();
-}
